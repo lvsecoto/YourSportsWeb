9b4acae24527d9ee3630831b50a145a7e47b9767
YuanJunyao <785570262@qq.com>
2018-03-16 11:30:14 +0800

    feat(situp): 可以添加和读取仰卧起坐记录

---
+++ routes/situp.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var situpCounter = 0;
+
+router.get('/', function(req, res, next) {
+  res.json(
+    {
+      'count': situpCounter.toString()
+    }
+  );
+});
+
+router.post('/', function (req, res) {
+  situpCounter++;
+
+  res.json({
+    'message' : 'success'
+  });
+ })
+
+module.exports = router;
